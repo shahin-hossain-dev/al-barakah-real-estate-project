23c451fc5491a427ec0e084d2d384c31c35ba67f
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-04-09 01:31:21 +0600

    login and Register form added and form handler added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,27 +1,40 @@
+import { Link } from "react-router-dom";
 import image1 from "../../assets/image1.webp";
 import Navbar from "../shared/Navbar/Navbar";
+import { useForm } from "react-hook-form";
+export const background = {
+  backgroundImage: `url(${image1})`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+};
 const Login = () => {
-  const background = {
-    backgroundImage: `url(${image1})`,
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const handleLogin = (userData) => {
+    console.log(userData);
   };
   return (
     <div style={background}>
       <Navbar />
 
-      <div className=" flex justify-center items-center min-h-screen bg-[#00000099]">
+      <div className=" flex justify-center items-center min-h-screen bg-[#0000004D]">
         <div className="card shrink-0 w-full max-w-sm shadow-2xl glass">
           <h1 className="text-3xl mt-5 text-center font-bold text-white">
             Login now!
           </h1>
-          <form className="card-body">
+          <form onSubmit={handleSubmit(handleLogin)} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text text-white">Email</span>
               </label>
               <input
+                {...register("email")}
                 type="email"
                 placeholder="email"
                 className="input input-bordered"
@@ -33,6 +46,7 @@ const Login = () => {
                 <span className="label-text text-white">Password</span>
               </label>
               <input
+                {...register("password")}
                 type="password"
                 placeholder="password"
                 className="input input-bordered"
@@ -48,9 +62,15 @@ const Login = () => {
               </label>
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary text-white">Login</button>
+              <button className="btn btn-neutral text-white">Login</button>
             </div>
           </form>
+          <p className="label-text-alt text-white text-center mb-8">
+            Don't have any account? please{" "}
+            <Link to={"/register"} className=" link link-hover link-warning">
+              Register
+            </Link>
+          </p>
         </div>
       </div>
     </div>
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,10 +1,93 @@
+import { useForm } from "react-hook-form";
 import Navbar from "../shared/Navbar/Navbar";
+import image1 from "../../assets/image1.webp";
+import { background } from "../Login/Login";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+  const { register, handleSubmit } = useForm();
+
+  const handleRegister = (userData) => {
+    console.log(userData);
+  };
   return (
-    <div>
+    <div style={background}>
       <Navbar />
-      <h2> This is Register page</h2>
+
+      <div className=" flex justify-center items-center min-h-screen bg-[#0000004D]">
+        <div className="card mt-24 shrink-0 w-full max-w-sm shadow-2xl glass">
+          <h1 className="text-3xl mt-5 text-center font-bold text-white">
+            Create Account
+          </h1>
+          <form onSubmit={handleSubmit(handleRegister)} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-white">Name</span>
+              </label>
+              <input
+                {...register("name")}
+                type="text"
+                placeholder="Name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-white">Photo URL</span>
+              </label>
+              <input
+                {...register("photoURL")}
+                type="text"
+                placeholder="Photo URL"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-white">Email</span>
+              </label>
+              <input
+                {...register("email")}
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-white">Password</span>
+              </label>
+              <input
+                {...register("password")}
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a
+                  href="#"
+                  className="label-text-alt link link-hover text-white"
+                >
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-neutral text-white">Register</button>
+            </div>
+          </form>
+          <p className="label-text-alt text-white text-center mb-8">
+            Already have an account? please{" "}
+            <Link to={"/login"} className=" link link-hover link-warning">
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
